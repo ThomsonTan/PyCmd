a498eeeafc222e60a2bbe54da5029494ff79138e
Tom Tan <Tom.Tan@microsoft.com>
2022-04-29 20:36:48 -0700

    Refactor cc command

--- command_cc.py
+++ command_cc.py
@@ -5,9 +5,6 @@ def run(tokens):
     cd_to_dir(tokens)
     
 def cd_to_dir(tokens):
-    curr_dir = os.getcwd()
-    pri_git_path = os.environ['PRIGIT']
-    pri_git_build_path = pri_git_path+'b'
     to_dir_id = -1
     if len(tokens) > 0 and tokens.isnumeric():
         to_dir_id = int(tokens)
@@ -21,12 +18,9 @@ def cd_to_dir(tokens):
 # -1 -> auto switch between source and 0
 # 0 -> build dir
 def get_to_dir(dir_id):
-    # TODO: support Linux which is case sensitive?
-    curr_dir = os.getcwd()
-    pri_git_path = os.environ['PRIGIT']
-    pri_git_build_path = pri_git_path+'b'
+    curr_dir, pri_git_dir, pri_git_build_dir = get_path()
 
-    (proj_name, dir_type) = get_proj_and_type()
+    proj_name, dir_type = get_proj_and_type()
 
     #TODO, for source dir, check CMakeLists.txt exists before switching?
 
@@ -35,16 +29,18 @@ def get_to_dir(dir_id):
 
     to_dir = ''
     if dir_id == -1:
+        # in source dir
         if dir_type == 0:
-            to_dir = os.path.join(pri_git_build_path, proj_name)
+            to_dir = os.path.join(pri_git_build_dir, proj_name)
             if not os.path.isdir(to_dir):
                 if not os.path.isfile(to_dir):
                     os.mkdir(to_dir)
                 else:
                     print(f"\n expected build dir {to_dir} is a file?")
                     # to_dir = ''
+        # in build dir
         elif dir_type == 1:
-            to_dir = os.path.join(pri_git_path, proj_name)
+            to_dir = os.path.join(pri_git_dir, proj_name)
             if not os.path.isdir(to_dir):
                 print(f"\nHow could this happen, source dir doesn't exist?")
         elif dir_type > 1:
@@ -53,28 +49,33 @@ def get_to_dir(dir_id):
 
     return to_dir
 
+def get_path():
+    curr_dir = os.getcwd()
+    pri_git_dir = os.environ['PRIGIT']
+    pri_git_build_dir = pri_git_dir + '_b'
+
+    return curr_dir, pri_git_dir, pri_git_build_dir
+
 # dir_type: 0 -> source
 #           1 -> build
 def get_proj_and_type():
-    curr_dir = os.getcwd()
-    pri_git_path = os.environ['PRIGIT']
-    pri_git_build_path = pri_git_path+'b'
+    curr_dir, pri_git_dir, pri_git_build_dir = get_path()
 
     curr_dir_lower = curr_dir.lower()
-    pri_git_path_lower = pri_git_path.lower()
-    pri_git_build_path_lower = pri_git_path.lower()+'b'
+    pri_git_dir_lower = pri_git_dir.lower()
+    pri_git_build_dir_lower = pri_git_build_dir.lower()
 
     proj_name = ''
     dir_type = -1
 
-    if curr_dir_lower.startswith(pri_git_path_lower + os.sep):
+    if curr_dir_lower.startswith(pri_git_dir_lower + os.sep):
         dir_type = 0
-        proj_name = curr_dir[len(pri_git_path)+1:]
+        proj_name = curr_dir[len(pri_git_dir)+1:]
         if (sep_pos := proj_name.find(os.sep)) != -1:
             proj_name = proj_name[:sep_pos]
-    elif curr_dir.startswith(pri_git_build_path_lower + os.sep):
+    elif curr_dir.startswith(pri_git_build_dir_lower + os.sep):
         dir_type = 1
-        proj_name = curr_dir[len(pri_git_build_path)+1:]
+        proj_name = curr_dir[len(pri_git_build_dir)+1:]
         if (sep_pos := proj_name.find(os.sep)) != -1:
             proj_name = proj_name[:sep_pos]
         if (sep_pos := proj_name.find('.')) != -1:
